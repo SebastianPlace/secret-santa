2c095ff201e3633028806e283551e3e913c816ed
Sebastian Place <sebplace94@gmail.com>
2017-10-21 17:36:15 +0100

    Refactor to promises for modularity

--- index.js
+++ index.js
@@ -3,32 +3,45 @@ const fs = require('fs')
 
 const FILE_PATH = './names.csv'
 
-function createPerson(headers, row) {
-  return headers
+function createPerson(keys, values) {
+  return keys
     .reduce((acc, cur, i) => {
-      const obj = {
-        [cur]: row[i]
-      }
+      const obj = { [cur]: values[i] }
       return Object.assign({}, acc, obj);
     }, {})
 }
 
-fs.readFile(FILE_PATH, 'utf8', (err, data) => {
-  if (err) { return console.error(err) }
-
-  csvParse(data, {}, (err, rows) => {
-    if (err) { return console.error(err) }
-    const headers = rows[0]
-    const body = rows.slice(1)
-    const people = body.map(row => { return createPerson(headers, row)} )
-    console.log(people)
-    // take an object, randomly select unpaired object and pair them
-    // what does a pair look like?
-    /* 
-    {
-      santa: personB
-      receiver: personA
-    }
-    */
+const readFile = filePath => new Promise((resolve, reject) => {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) { reject(err) }
+    else { resolve(data) }
   })
 })
+
+const parseCSV = data => new Promise((resolve, reject) => {
+  csvParse(data, {}, (err, rows) => {
+    if (err) { reject(err) }
+    else {
+      const headers = rows[0]
+      const body = rows.slice(1)
+      const people = body.map(row => { return createPerson(headers, row)} )
+      resolve(people);
+    }
+  })
+})
+
+readFile(FILE_PATH)
+  .then(parseCSV.bind(this))
+  .then(people => console.log(people))
+
+
+// take an object, randomly select unpaired object and pair them
+// what does a pair look like?
+// everyone needs to be a giver
+// everyone needs to be a receiver
+/* 
+{
+  giver: personB
+  receiver: personA
+}
+*/
